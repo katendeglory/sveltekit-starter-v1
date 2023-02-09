3f78799b85591190a3f1ac76d71b21fa84d70c07
Glory Katende <katendeglory@gmail.com>
2023-02-09 15:29:50 +0200

    Yet another commit

--- src/components/layout/MobileMenu.svelte
+++ src/components/layout/MobileMenu.svelte
@@ -15,7 +15,7 @@
 {#if menuVisible}
   <div
     on:click|self={hideMenu}
-    class="z-[850] bg-gray-500/5 fixed top-0 left-0 bottom-0 right-0"
+    class="z-[850] bg-gray-500/5 fixed top-0 left-0 bottom-0 right-0 w-screen h-screen"
   />
 {/if}
 
